7678854de7e6ee4a8a4fff63373a41ff62d895c9
citwazabi <citwazabi@gmail.com>
2022-02-14 17:22:24 +0700

    14022022 17.22

--- app/src/main/java/com/example/mrta_app_parking_mobile_v1/activity/InCarMainActivity.java
+++ app/src/main/java/com/example/mrta_app_parking_mobile_v1/activity/InCarMainActivity.java
@@ -1,15 +1,95 @@
 package com.example.mrta_app_parking_mobile_v1.activity;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.mrta_app_parking_mobile_v1.R;
 
-public class InCarMainActivity extends AppCompatActivity {
+public class InCarMainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_in_car_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setTitle("ตั้งค่าเครื่อง");
+
+        if (getSupportActionBar() != null) {
+
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        }
+
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_clear, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                Intent intent = new Intent(InCarMainActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+                break;
+
+            default:
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+
+        Intent intent = new Intent(InCarMainActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+
+
+
+
+
+
 }
--- app/src/main/java/com/example/mrta_app_parking_mobile_v1/activity/MainActivity.java
+++ app/src/main/java/com/example/mrta_app_parking_mobile_v1/activity/MainActivity.java
@@ -1,15 +1,135 @@
 package com.example.mrta_app_parking_mobile_v1.activity;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.mrta_app_parking_mobile_v1.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+
+
+    private DrawerLayout drawer;
+    CardView btn_gate_in;
+    CardView btn_gate_out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        toolbar.setTitle("Parking");
+
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        View headerView = navigationView.getHeaderView(0);
+        TextView navUsername = (TextView) headerView.findViewById(R.id.txt_name);
+        navUsername.setText("User Admin");
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        inintInstances();
     }
+
+
+    private void inintInstances() {
+
+
+        btn_gate_in = findViewById(R.id.btn_gate_in);
+        btn_gate_out = findViewById(R.id.btn_gate_out);
+
+
+        btn_gate_in.setOnClickListener(this);
+        btn_gate_out.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main_clear, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        // handle arrow click here
+
+
+        switch (id) {
+
+
+
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.item_setting:
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+                finish();
+                break;
+
+            case R.id.item_history:
+
+                break;
+
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        if(v == btn_gate_in){
+
+            startActivity(new Intent(MainActivity.this, InCarMainActivity.class));
+            finish();
+
+        }else if(v == btn_gate_out){
+
+            startActivity(new Intent(MainActivity.this, InCarMainActivity.class));
+            finish();
+        }
+
+    }
+
+
+
+
+
+
+
 }
